e721dd81ce3bd3830d8ec35a54d34b493964c89b
David LJ <mail@davidlj95.com>
2023-10-16 22:20:45 +0200

    style: apply format

--- src/app/home-page/home-page.component.spec.ts
+++ src/app/home-page/home-page.component.spec.ts
@@ -1,21 +1,21 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { HomePageComponent } from './home-page.component';
+import { HomePageComponent } from './home-page.component'
 
 describe('HomePageComponent', () => {
-  let component: HomePageComponent;
-  let fixture: ComponentFixture<HomePageComponent>;
+  let component: HomePageComponent
+  let fixture: ComponentFixture<HomePageComponent>
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [HomePageComponent]
-    });
-    fixture = TestBed.createComponent(HomePageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+      declarations: [HomePageComponent],
+    })
+    fixture = TestBed.createComponent(HomePageComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
