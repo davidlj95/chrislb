073b14cc92373d023e276a2f7fe28d7a0d32bc96
David LJ <mail@davidlj95.com>
2023-10-16 18:08:20 +0200

    build: add lighthouse config

---
+++ lighthouserc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  ci: {
+    upload: {
+      target: 'temporary-public-storage',
+      url: ['http://localhost/'],
+    },
+    collect: {
+      staticDistDir: process.env.CI ? '.' : 'dist/chrislb/browser',
+    },
+    assert: {
+      preset: 'lighthouse:no-pwa',
+      assertions: {
+        'bf-cache': 'warn',
+        'csp-xss': 'warn',
+        'unused-javascript': 'warn',
+        'uses-responsive-images': 'warn',
+      },
+    },
+  },
+}
