7e037892c74f57da050a03c9e2ff21ca86f0abac
David LJ <mail@davidlj95.com>
2023-10-16 19:33:08 +0200

    refactor: add routing with home page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+import { HomePageComponent } from './home-page/home-page.component'
 
-const routes: Routes = []
+const routes: Routes = [
+  { path: '', component: HomePageComponent, pathMatch: 'full' },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+]
 
 @NgModule({
   imports: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomePageComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
