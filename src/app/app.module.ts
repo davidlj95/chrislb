2dd4e7ad6ff05fbd21643a2fe93356d0eb87ceb3
David LJ <mail@davidlj95.com>
2023-10-16 22:19:21 +0200

    feat: add muchas cosas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { HomePageComponent } from './home-page/home-page.component'
+import { HeaderComponent } from './home-page/header/header.component'
+import { ProjectsComponent } from './home-page/projects/projects.component'
+import { ProjectItemComponent } from './home-page/projects/project-item/project-item.component'
+import { NgOptimizedImage } from '@angular/common'
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    HeaderComponent,
+    ProjectsComponent,
+    ProjectItemComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, NgOptimizedImage],
   providers: [],
   bootstrap: [AppComponent],
 })
