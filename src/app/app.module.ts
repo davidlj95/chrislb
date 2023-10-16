01e0d99d64e8b3a159fc25ed24fb34f8fd9500d4
David LJ <mail@davidlj95.com>
2023-10-16 22:23:30 +0200

    refactor: add routing with home page component (#6)
    * refactor: add routing with home page component
    
    * style: apply format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { HomePageComponent } from './home-page/home-page.component'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomePageComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
