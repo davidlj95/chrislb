8e5ab9f94f050be0896282828146fd5f8d4fd1a4
David LJ <mail@davidlj95.com>
2023-10-19 20:02:16 +0200

    test: add optimized image to logo

--- src/app/logo/logo.component.spec.ts
+++ src/app/logo/logo.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { LogoComponent } from './logo.component'
+import { NgOptimizedImage } from '@angular/common'
 
 describe('LogoComponent', () => {
   let component: LogoComponent
@@ -8,6 +9,7 @@ describe('LogoComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [LogoComponent],
+      imports: [NgOptimizedImage],
     })
     fixture = TestBed.createComponent(LogoComponent)
     component = fixture.componentInstance
