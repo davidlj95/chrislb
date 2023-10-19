18a960c466eadea21d0bbbf250d345ba9e3538d3
David LJ <mail@davidlj95.com>
2023-10-19 19:38:23 +0200

    chore: fix linting issue unused import

--- src/app/home-page/home-page.component.spec.ts
+++ src/app/home-page/home-page.component.spec.ts
@@ -3,7 +3,6 @@ import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { HomePageComponent } from './home-page.component'
 import { MockComponents } from 'ng-mocks'
 import { ProjectsComponent } from './projects/projects.component'
-import { HeaderComponent } from '../header/header.component'
 
 describe('HomePageComponent', () => {
   let component: HomePageComponent
