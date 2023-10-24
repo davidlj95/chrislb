8b6174b88f36b55b26e5a9245d885b1705a0d008
David LJ <mail@davidlj95.com>
2023-10-24 06:54:26 +0200

    build: enable lighthouse for more routes

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,10 +2,15 @@ module.exports = {
   ci: {
     upload: {
       target: 'temporary-public-storage',
-      url: ['http://localhost/'],
     },
     collect: {
+      numberOfRuns: 3,
       staticDistDir: process.env.CI ? '.' : 'dist/chrislb/browser',
+      url: [
+        'http://localhost/',
+        'http://localhost/projects/chiasma/',
+        'http://localhost/404',
+      ],
     },
     assert: {
       preset: 'lighthouse:no-pwa',
