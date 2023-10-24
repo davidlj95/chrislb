e1c3c2aa7a4c708281905ba9c3673f04aa062580
David LJ <mail@davidlj95.com>
2023-10-24 02:28:41 +0200

    feat: add animations (#22)
    * feat: add animations
    
    * feat: update theme color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core'
+import { ENTER_LEAVE_FADE_IN_OUT_ANIMATIONS } from './common/animations'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  animations: [
+    ENTER_LEAVE_FADE_IN_OUT_ANIMATIONS.children('routeAnimations'),
+    ENTER_LEAVE_FADE_IN_OUT_ANIMATIONS.element('enterLeave'),
+  ],
 })
 export class AppComponent {}
