66dd3b6513e4bfa8fcc5b41577d77438221251b4
David LJ <mail@davidlj95.com>
2023-10-29 02:56:27 +0200

    feat(projects): use authors for credits

--- src/app/projects-page/project-item/project-item.component.ts
+++ src/app/projects-page/project-item/project-item.component.ts
@@ -1,18 +1,29 @@
-import { Component, Input, OnChanges } from '@angular/core'
-import { ProjectItem } from './project-item'
+import { Component, Input } from '@angular/core'
+import { Credit, ProjectItem } from './project-item'
 import { SwiperOptions } from 'swiper/types'
 import { ImageAsset } from '../../../data/images/types'
 import { ProjectPreviewImagesService } from './project-preview-images.service'
 import { PROJECTS_PATH } from '../../routes'
 import { ImageResponsiveBreakpointsService } from '../../common/image-responsive-breakpoints.service'
+import { Author, AuthorsService } from '../../common/authors.service'
 
 @Component({
   selector: 'app-project-item',
   templateUrl: './project-item.component.html',
   styleUrls: ['./project-item.component.scss'],
 })
-export class ProjectItemComponent implements OnChanges {
-  @Input({ required: true }) public item!: ProjectItem
+export class ProjectItemComponent {
+  protected _item!: ProjectItem
+  @Input({ required: true })
+  public set item(item: ProjectItem) {
+    this._item = item
+    this.previewImages = this.projectPreviewImagesService.bySlug(item.slug)
+    this.credits = item.credits.map((credit) => ({
+      ...credit,
+      author: this.authorsService.bySlug(credit.slug),
+    }))
+  }
+
   @Input() public priority?: boolean
   public readonly CUSTOM_SWIPER_OPTIONS: SwiperOptions = {
     slidesPerView: 2,
@@ -24,14 +35,16 @@ export class ProjectItemComponent implements OnChanges {
       this.imageResponsiveBreakpointsService.MAX_SCREEN_WIDTH_PX / 3,
     )
     .toSrcSet()
+  public credits!: ReadonlyArray<CreditItem>
   protected readonly PROJECTS_PATH = PROJECTS_PATH
 
   constructor(
     private projectPreviewImagesService: ProjectPreviewImagesService,
     private imageResponsiveBreakpointsService: ImageResponsiveBreakpointsService,
+    private authorsService: AuthorsService,
   ) {}
+}
 
-  ngOnChanges(): void {
-    this.previewImages = this.projectPreviewImagesService.bySlug(this.item.slug)
-  }
+type CreditItem = Omit<Credit, 'slug'> & {
+  author: Author | undefined
 }
--- src/app/projects-page/project-item/project-item.ts
+++ src/app/projects-page/project-item/project-item.ts
@@ -11,4 +11,5 @@ export interface Credit {
   readonly role: string
   readonly name: string
   readonly nickname: string
+  readonly slug: string
 }
