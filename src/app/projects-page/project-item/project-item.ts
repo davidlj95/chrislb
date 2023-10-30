b446a10b9f5b4cf6f67ba0704e9d86169a9d6fdf
David LJ <mail@davidlj95.com>
2023-10-30 23:15:44 +0100

    feat(projects): allow for projects without preview images

--- src/app/projects-page/project-item/project-item.ts
+++ src/app/projects-page/project-item/project-item.ts
@@ -6,11 +6,13 @@ export interface Project {
   readonly subtitle: string
   readonly quote?: string
   readonly description: string
-  readonly credits: readonly Credit[]
+  //👇 When hasn't been set, CMS doesn't set the property
+  //   CMS sets it after though (if adding & removing)
+  readonly credits?: readonly Credit[]
 }
 
 export interface ProjectListItem extends Project {
-  readonly previewImages: ReadonlyArray<ImageAsset>
+  readonly previewImages?: ReadonlyArray<ImageAsset>
 }
 
 export interface Credit {
