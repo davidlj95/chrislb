4cfed3fa4951ac8d6b7f2ff9dde55ed9a3883958
David LJ <mail@davidlj95.com>
2023-11-05 17:22:58 +0100

    docs(content): update guide with new changes (#120)

--- src/app/common/directories.ts
+++ src/app/common/directories.ts
@@ -1,5 +1,5 @@
 // 👇 Used by script, despite marked unused by IDE
 export const DATA_DIR = 'data'
 export const CONTENTS_DIR = 'content'
-// Must match directory name here and in the images CDN (for scripts to work)
+// Must match directory name here and in the image CDN (for scripts to work)
 export const PROJECTS_DIR = 'projects'
