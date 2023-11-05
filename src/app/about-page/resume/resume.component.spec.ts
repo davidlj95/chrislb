1891334d1bebc0c4b10d557c1db46bf2b40ba644
David LJ <mail@davidlj95.com>
2023-11-05 18:33:14 +0100

    refactor(about): resume service injected in module (#123)

--- src/app/about-page/resume/resume.component.spec.ts
+++ src/app/about-page/resume/resume.component.spec.ts
@@ -1,8 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { ResumeComponent } from './resume.component'
-import { MockComponents } from 'ng-mocks'
+import { MockComponents, MockProvider } from 'ng-mocks'
 import { FaIconComponent } from '@fortawesome/angular-fontawesome'
+import { ResumeService } from './resume.service'
 
 describe('ResumeComponent', () => {
   let component: ResumeComponent
@@ -11,6 +12,7 @@ describe('ResumeComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ResumeComponent, MockComponents(FaIconComponent)],
+      providers: [MockProvider(ResumeService)],
     })
     fixture = TestBed.createComponent(ResumeComponent)
     component = fixture.componentInstance
