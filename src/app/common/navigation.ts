b0606a1ee3fd757c2c9dae70c979e177ede1b1fc
David LJ <mail@davidlj95.com>
2023-11-06 23:17:06 +0100

    chore: remove unused navigation file

--- src/app/common/navigation.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from '@angular/router'
-import { NOT_FOUND_PATH } from '../routes'
-
-export function displayNotFound(router: Router) {
-  return router.navigate([NOT_FOUND_PATH], { skipLocationChange: true })
-}
