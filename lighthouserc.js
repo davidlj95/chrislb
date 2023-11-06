fa46767c62f6b99ab157737c93efe98c777ccc97
David LJ <mail@davidlj95.com>
2023-11-06 01:00:23 +0100

    perf: enable source maps Lighthouse assertion

--- lighthouserc.js
+++ lighthouserc.js
@@ -18,9 +18,10 @@ module.exports = {
       assertions: {
         //👇 Didn't appear til main bundle was >500kb. Then, Lighthouse wants source maps
         // If we optimize, we can probably turn this back on. 500kb for main bundle is a bit too much
-        'valid-source-maps': 'off',
+        // Now we're below 500kb, enabling it again by leaving it commented in case it's bigger
+        // 'valid-source-maps': 'off',
         // 👇 Probably Swiper.js 😭
-        // Just happens on project detail page tho, when many swipers in there
+        // Just happens on project detail page tho, when many swipers there
         // Maybe a grid would solve it
         'non-composited-animations': 'warn',
         'bf-cache': 'warn',
