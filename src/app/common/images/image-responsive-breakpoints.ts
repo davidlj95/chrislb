22dd29b0a678c3febfdbc19402fbdc00155e6c47
David LJ <mail@davidlj95.com>
2023-11-09 11:44:41 +0100

    chore(images): remove old breakpoints service (#164)

--- src/app/common/images/image-responsive-breakpoints.ts
+++
@@ -1,7 +0,0 @@
-export class ImageResponsiveBreakpoints {
-  constructor(public readonly pxValues: ReadonlyArray<number>) {}
-
-  public toSrcSet(): string {
-    return this.pxValues.map((breakpoint) => `${breakpoint}w`).join(', ')
-  }
-}
