78c5662129785a57788b24a00eb798e1325c8f15
David LJ <mail@davidlj95.com>
2023-11-09 11:51:31 +0100

    refactor(images): implement constrained method with own methods (#165)

--- src/app/logo/logo.component.ts
+++ src/app/logo/logo.component.ts
@@ -23,7 +23,9 @@ export class LogoComponent {
     const maxWidthPx = aspectRatio * this.LOGO_MAX_HEIGHT_PX
     this.horizontalLogo = new ResponsiveImage(
       miscImages.horizontalLogo,
-      responsiveImageAttributesService.constrained(Px(Math.ceil(maxWidthPx))),
+      responsiveImageAttributesService.fullWidthUntil(
+        Px(Math.ceil(maxWidthPx)),
+      ),
     )
   }
 }
