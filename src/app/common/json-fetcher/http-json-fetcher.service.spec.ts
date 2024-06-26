b014d14f64809ca8898554b89966b6368b387d00
David LJ <mail@davidlj95.com>
2024-06-26 12:27:00 +0200

    build: update Angular to v18 (#404)
    * build: update Angular to v18
    
    * build: configure Renovate for Angular v18
    
    * build(deps): update @fortawesome/angular-fontawesome to v18 compat
    
    * build: configure Renovate to keep in v18

--- src/app/common/json-fetcher/http-json-fetcher.service.spec.ts
+++ src/app/common/json-fetcher/http-json-fetcher.service.spec.ts
@@ -1,17 +1,22 @@
 import { TestBed } from '@angular/core/testing'
 
 import { HttpJsonFetcherService } from './http-json-fetcher.service'
-import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { provideHttpClientTesting } from '@angular/common/http/testing'
 import { MockProvider } from 'ng-mocks'
 import { APP_BASE_HREF } from '@angular/common'
+import { provideHttpClient } from '@angular/common/http'
 
 describe('HttpJsonFetcherService', () => {
   let service: HttpJsonFetcherService
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      providers: [MockProvider(APP_BASE_HREF, '/')],
+      imports: [],
+      providers: [
+        MockProvider(APP_BASE_HREF, '/'),
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ],
     })
     service = TestBed.inject(HttpJsonFetcherService)
   })
