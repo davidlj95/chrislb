b26bf7256a116d57a834c7ef720beaa03bb716c4
David LJ <mail@davidlj95.com>
2024-11-18 13:10:28 +0100

    build(lint): ignore files from `.gitignore`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,16 @@ import eslint from '@eslint/js'
 import tsEslint from 'typescript-eslint'
 import angular from 'angular-eslint'
 
+import { includeIgnoreFile } from '@eslint/compat'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const gitignorePath = path.resolve(__dirname, '.gitignore')
+
 export default tsEslint.config(
+  includeIgnoreFile(gitignorePath),
   {
     files: ['**/*.ts'],
     extends: [
