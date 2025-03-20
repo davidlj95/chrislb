09c34ea3886c5dd5fefc065af594d96c475bdb97
David LJ <mail@davidlj95.com>
2025-03-20 04:43:08 +0100

    refactor: split main routes into modules

---
+++ src/app/not-found-page/not-found-page.routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router'
+import { NOT_FOUND_PATH } from '../common/routing/paths'
+import { NotFoundPageComponent } from './not-found-page.component'
+import { makeRouteMetadata } from '../common/routing/make-route-metadata'
+import notFoundPageMetadata from '../../data/pages/404.json'
+
+export const NOT_FOUND_PAGE_ROUTES: Routes = [
+  {
+    path: NOT_FOUND_PATH,
+    component: NotFoundPageComponent,
+    data: makeRouteMetadata(notFoundPageMetadata),
+  },
+  {
+    path: '**',
+    component: NotFoundPageComponent,
+    data: makeRouteMetadata(notFoundPageMetadata, [NOT_FOUND_PATH]),
+  },
+]
--- src/app/projects/projects.routes.ts
+++ src/app/projects/projects.routes.ts
@@ -6,15 +6,30 @@ import { SLUG_PARAM } from './projects.routes-params'
 import { ProjectRouteData } from './project-detail-page/projects-routes-data'
 import { RouteDataResolver } from '../common/routing/route-data-resolver'
 import { ProjectsService } from './projects.service'
+import { ProjectsListPageComponent } from './projects-list-page/projects-list-page.component'
+import { makeRouteMetadata } from '../common/routing/make-route-metadata'
+import projectsListPageMetadata from '../../data/pages/projects-list.json'
+import { PROJECTS_PATH } from '../common/routing/paths'
 
 export const PROJECTS_ROUTES: Route[] = [
-  { path: '', redirectTo: '/', pathMatch: 'full' },
   {
-    path: `:${SLUG_PARAM}`,
-    component: ProjectDetailPageComponent,
-    resolve: {
-      project: (route) => inject(ProjectDetailPageResolver).project(route),
-    } as RouteDataResolver<ProjectRouteData>,
-    providers: [ProjectDetailPageResolver, ProjectsService],
+    path: '',
+    component: ProjectsListPageComponent,
+    data: makeRouteMetadata(projectsListPageMetadata),
+    pathMatch: 'full',
+  },
+  {
+    path: PROJECTS_PATH,
+    children: [
+      { path: '', redirectTo: '/', pathMatch: 'full' },
+      {
+        path: `:${SLUG_PARAM}`,
+        component: ProjectDetailPageComponent,
+        resolve: {
+          project: (route) => inject(ProjectDetailPageResolver).project(route),
+        } as RouteDataResolver<ProjectRouteData>,
+        providers: [ProjectDetailPageResolver, ProjectsService],
+      },
+    ],
   },
 ]
