f72100d29579ff951d5559cf071ca332eb7eb74b
David LJ <mail@davidlj95.com>
2025-03-20 04:47:37 +0100

    refactor: split main routes into modules (#558)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,40 +1,14 @@
-import { ProjectsListPageComponent } from './projects/projects-list-page/projects-list-page.component'
-import { NotFoundPageComponent } from './not-found-page/not-found-page.component'
-import projectsListPageMetadata from '../data/pages/projects-list.json'
-import notFoundPageMetadata from '../data/pages/404.json'
-import { makeRouteMetadata } from './common/routing/make-route-metadata'
-import {
-  ABOUT_PATH,
-  NOT_FOUND_PATH,
-  PROJECTS_PATH,
-} from './common/routing/paths'
-import { PROJECTS_ROUTES } from './projects/projects.routes'
 import { Routes } from '@angular/router'
+import { PROJECTS_ROUTES } from './projects/projects.routes'
+import { ABOUT_PATH } from './common/routing/paths'
+import { NOT_FOUND_PAGE_ROUTES } from './not-found-page/not-found-page.routes'
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: ProjectsListPageComponent,
-    data: makeRouteMetadata(projectsListPageMetadata),
-    pathMatch: 'full',
-  },
-  {
-    path: PROJECTS_PATH,
-    children: PROJECTS_ROUTES,
-  },
+  ...PROJECTS_ROUTES,
   {
     path: ABOUT_PATH,
     loadChildren: () =>
       import('./about-page/about-page.routes').then((m) => m.ABOUT_ROUTES),
   },
-  {
-    path: NOT_FOUND_PATH,
-    component: NotFoundPageComponent,
-    data: makeRouteMetadata(notFoundPageMetadata),
-  },
-  {
-    path: '**',
-    component: NotFoundPageComponent,
-    data: makeRouteMetadata(notFoundPageMetadata, [NOT_FOUND_PATH]),
-  },
+  ...NOT_FOUND_PAGE_ROUTES,
 ]
