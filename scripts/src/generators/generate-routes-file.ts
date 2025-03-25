a0d0a7fa8d135774ee16e5ed6305a9ddf9f4420b
David LJ <mail@davidlj95.com>
2025-03-25 02:16:07 +0100

    refactor: routes file script into functional style (#565)

---
+++ scripts/src/generators/generate-routes-file.ts
@@ -0,0 +1,49 @@
+import { isMain } from '../utils/is-main'
+import { Log } from '../utils/log'
+import { join } from 'path'
+import { PROJECTS_DIR } from '../../../src/app/common/directories'
+import { ProjectListItem } from '../../../src/app/projects/project'
+import { appendJsonExtension, readJson } from '../resources/json'
+import { CONTENT_PATH, REPO_PATH } from '../utils/paths'
+import { readFile, writeFile } from 'fs/promises'
+
+export const generateRoutesFile = async (): Promise<void> => {
+  Log.group('Routes file')
+
+  Log.info("Reading base routes file '%s'", BASE_ROUTES_FILENAME)
+  const baseRoutesFile = await readFile(
+    join(REPO_PATH, BASE_ROUTES_FILENAME),
+    'utf-8',
+  )
+  const baseRoutes = baseRoutesFile.split('\n').filter((route) => route.length)
+  Log.info('Found %d base routes', baseRoutes.length)
+
+  Log.info('Listing projects with contents')
+  const projectListJson = await readJson<readonly ProjectListItem[]>(
+    join(CONTENT_PATH, appendJsonExtension(PROJECTS_DIR)),
+  )
+  const projectSlugsWithContent = projectListJson
+    .filter((projectListItem) => projectListItem.hasDetails)
+    .map(({ slug }) => slug)
+  Log.info('Found %d projects with contents', projectSlugsWithContent.length)
+
+  projectSlugsWithContent.forEach((slug) => Log.item(slug))
+  const projectRoutes = projectSlugsWithContent.map(
+    (projectSlug) => `/projects/${projectSlug}`,
+  )
+
+  Log.info('Generated routes')
+  projectRoutes.forEach((route) => Log.item(route))
+  const allRoutes = [...baseRoutes, ...projectRoutes].sort()
+
+  Log.info('Writing all routes (%d)', allRoutes.length)
+  await writeFile(join(REPO_PATH, ROUTES_FILENAME), allRoutes.join('\n'))
+}
+
+const BASE_ROUTES_FILENAME = 'routes-file.base.txt'
+const ROUTES_FILENAME = 'routes-file.txt'
+
+if (isMain(import.meta.url)) {
+  await generateRoutesFile()
+  Log.ok('All done')
+}
