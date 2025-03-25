b94db4291c0e5186952f094bfb95cb763389fe1b
David LJ <mail@davidlj95.com>
2025-03-25 18:04:30 +0100

    build: add path mappings to improve JSON data imports

--- src/app/common/routing/paths.ts
+++ src/app/common/routing/paths.ts
@@ -1,6 +1,6 @@
-import projectsListPageMetadata from '../../../../data/cms/pages/projects-list.json'
-import notFoundPageMetadata from '../../../../data/cms/pages/404.json'
-import aboutPageMetadata from '../../../../data/cms/pages/about.json'
+import projectsListPageMetadata from '@/data/cms/pages/projects-list.json'
+import notFoundPageMetadata from '@/data/cms/pages/404.json'
+import aboutPageMetadata from '@/data/cms/pages/about.json'
 
 export const PROJECTS_PATH = projectsListPageMetadata.slug
 export const NOT_FOUND_PATH = notFoundPageMetadata.slug
