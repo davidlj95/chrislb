3845ebd9fa3345a2423ae8c1293eb79a3ff656c8
David LJ <mail@davidlj95.com>
2025-03-25 17:21:17 +0100

    chore: update paths to use existing path as base

--- scripts/src/utils/paths.ts
+++ scripts/src/utils/paths.ts
@@ -18,11 +18,7 @@ const SRC_PATH = join(REPO_PATH, 'src')
 const PUBLIC_PATH = join(REPO_PATH, 'public')
 export const DATA_PATH = join(SRC_PATH, DATA_DIR)
 export const CONTENT_PATH = join(PUBLIC_PATH, CONTENTS_DIR)
-export const PROJECTS_CONTENT_PATH = join(
-  PUBLIC_PATH,
-  CONTENTS_DIR,
-  PROJECTS_DIR,
-)
+export const PROJECTS_CONTENT_PATH = join(CONTENT_PATH, PROJECTS_DIR)
 export const MISC_PATH = join(DATA_PATH, 'misc')
 export const ALBUM_PRESETS_PATH = join(DATA_PATH, 'album-presets')
 export const AUTHORS_PATH = join(DATA_PATH, 'authors')
