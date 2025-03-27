db753b413c97a9993ecfa1e3e16e3757bc3b04f3
David LJ <mail@davidlj95.com>
2025-03-27 02:20:35 +0100

    ci: add missing secrets to prebuild steps

--- scripts/src/images/cloudinary.ts
+++ scripts/src/images/cloudinary.ts
@@ -1,9 +1,5 @@
 import { ImageCdnApi, UNPUBLISHED_TAG } from './image-cdn-api'
-import {
-  ConfigOptions,
-  ResourceApiResponse,
-  v2 as cloudinary,
-} from 'cloudinary'
+import { ConfigOptions, v2 as cloudinary } from 'cloudinary'
 import dotenv from 'dotenv'
 import { Log } from '../utils/log'
 import { CLOUDINARY_CLOUD_NAME } from '../../../src/app/common/images/cdn-config'
@@ -33,9 +29,7 @@ export class Cloudinary implements ImageCdnApi {
     })
   }
 
-  async getAllImagesInPath(
-    path: string,
-  ): Promise<readonly CloudinaryImageAsset[]> {
+  async getAllImagesInPath(path: string): Promise<readonly ImageAsset[]> {
     const imageAssets = await this._listImageAssetsInPath(path)
     Log.info('Found %d images in path "%s"', imageAssets.length, path)
     return imageAssets
@@ -43,30 +37,30 @@ export class Cloudinary implements ImageCdnApi {
 
   private async _listImageAssetsInPath(
     path: string,
-  ): Promise<readonly CloudinaryImageAsset[]> {
+  ): Promise<readonly ImageAsset[]> {
     const response = await cloudinary.api.resources_by_asset_folder(path, {
       max_results: 50, // the default right now, but to be specific & consistent over time
       resource_type: 'image',
-      fields: 'width,height,version,tags', // public_id and asset_id are always included
+      fields: 'width,height,tags', // public_id and asset_id are always included
       tags: true,
     })
     return response.resources
       .filter((resource) => !resource.tags.includes(UNPUBLISHED_TAG))
       .toSorted((a, b) => (a.public_id < b.public_id ? -1 : 1))
-      .map(({ public_id, width, height, version, asset_id }) => ({
+      .map(({ public_id, width, height }) => ({
         filename: public_id,
         width,
         height,
-        version,
-        asset_id,
+        //version,
+        //asset_id,
       }))
   }
 }
 
-type Unpacked<T> = T extends (infer U)[] ? U : T
-type ResourceApiResponseItem = Unpacked<ResourceApiResponse['resources']>
-type CloudinaryImageAsset = ImageAsset &
-  Pick<ResourceApiResponseItem, 'version'> & {
-    // TODO: could be included, as it's in the actual response, but not in the response type
-    asset_id: string
-  }
+//type Unpacked<T> = T extends (infer U)[] ? U : T
+//type ResourceApiResponseItem = Unpacked<ResourceApiResponse['resources']>
+//type CloudinaryImageAsset = ImageAsset
+//& Pick<ResourceApiResponseItem, 'version'> & {
+// TODO: could be included, as it's in the actual response, but not in the response type
+//asset_id: string
+//}
