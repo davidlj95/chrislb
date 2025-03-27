c016b7d47861c6db567db52c2841d11efa04adc7
David LJ <mail@davidlj95.com>
2025-03-27 21:32:57 +0100

    refactor: lazy load routes (#592)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,28 @@
 import { Routes } from '@angular/router'
-import { PROJECTS_ROUTES } from './projects/projects.routes'
-import { ABOUT_PATH } from './common/routing/paths'
-import { NOT_FOUND_PAGE_ROUTES } from './not-found-page/not-found-page.routes'
+import { ABOUT_PATH, PROJECTS_PATH } from './common/routing/paths'
 
 export const routes: Routes = [
-  ...PROJECTS_ROUTES,
+  {
+    path: '',
+    pathMatch: 'full',
+    loadChildren: () =>
+      import('./projects/projects.routes').then((m) => m.PROJECTS_LIST_ROUTES),
+  },
+  {
+    path: PROJECTS_PATH,
+    loadChildren: () =>
+      import('./projects/projects.routes').then((m) => m.PROJECTS_ROUTES),
+  },
   {
     path: ABOUT_PATH,
     loadChildren: () =>
       import('./about-page/about-page.routes').then((m) => m.ABOUT_ROUTES),
   },
-  ...NOT_FOUND_PAGE_ROUTES,
+  {
+    path: '**',
+    loadChildren: () =>
+      import('./not-found-page/not-found-page.routes').then(
+        (m) => m.NOT_FOUND_PAGE_ROUTES,
+      ),
+  },
 ]
