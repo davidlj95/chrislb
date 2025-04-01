518656b94528fb2504abe6d59717e5d7fbb628c9
David LJ <mail@davidlj95.com>
2025-04-01 20:58:28 +0200

    fix: add link rel preconnect only in ssg/r

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { afterNextRender, Component, inject } from '@angular/core'
+import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core'
 import { ENTER_LEAVE_FADE_IN_OUT_ANIMATIONS } from './common/style/animations'
 import { RouterOutlet } from '@angular/router'
 import { LogoComponent } from './logo/logo.component'
 import { HeaderComponent } from './header/header.component'
-import { DOCUMENT } from '@angular/common'
+import { DOCUMENT, isPlatformServer } from '@angular/common'
 import { BASE_URL as IMAGE_CDN_BASE_URL } from '../app/common/images/cdn'
 
 @Component({
@@ -17,14 +17,15 @@ import { BASE_URL as IMAGE_CDN_BASE_URL } from '../app/common/images/cdn'
   standalone: true,
   imports: [HeaderComponent, LogoComponent, RouterOutlet],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private _platform = inject(PLATFORM_ID)
   private _doc = inject(DOCUMENT)
 
-  constructor() {
+  ngOnInit() {
     // Just needed for SSG/R
-    afterNextRender(() => {
+    if (isPlatformServer(this._platform)) {
       appendLinkRelPreconnect(this._doc, IMAGE_CDN_BASE_URL)
-    })
+    }
   }
 }
 
