1145e1a48c17baa210bf2d2909ec7859e5fdaf89
David LJ <mail@davidlj95.com>
2025-04-02 18:37:15 +0200

    test: enable back lighthouse responsive images audit

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -13,23 +13,34 @@ module.exports = {
       ],
     },
     assert: {
-      preset: 'lighthouse:no-pwa',
-      assertions: {
-        //👇 Didn't appear til main bundle was >500kb. Then, Lighthouse wants source maps
-        // If we optimize, we can probably turn this back on. 500kb for main bundle is a bit too much
-        // ~Now we're below 500kb, enabling it again by leaving it commented in case it's bigger~
-        'valid-source-maps': 'off',
-        // 👇 Probably Swiper.js 😭
-        // Just happens on project detail page tho, when many swipers there
-        // Maybe a grid would solve it
-        'non-composited-animations': 'warn',
-        //👇 WIP
-        //   They're not so bad, but can be improved. Disabling til solution is shipped.
-        'uses-responsive-images': 'warn',
-        'bf-cache': 'warn',
-        'csp-xss': 'warn',
-        'unused-javascript': 'warn',
-      },
+      assertMatrix: [
+        // All pages
+        {
+          matchingUrlPattern: '.*',
+          preset: 'lighthouse:no-pwa',
+          assertions: {
+            //👇 Didn't appear til main bundle was >500kb. Then, Lighthouse wants source maps
+            // If we optimize, we can probably turn this back on. 500kb for main bundle is a bit too much
+            // ~Now we're below 500kb, enabling it again by leaving it commented in case it's bigger~
+            'valid-source-maps': 'off',
+            'uses-responsive-images': ['error', { maxLength: 2 }],
+            // 👇 Probably Swiper.js 😭
+            // Just happens on project detail page tho, when many swipers there
+            // Maybe a grid would solve it
+            'non-composited-animations': 'warn',
+            'bf-cache': 'warn',
+            'csp-xss': 'warn',
+            'unused-javascript': 'warn',
+          },
+        },
+        // Project detail
+        {
+          matchingUrlPattern: '.+\/projects\/.+',
+          assertions: {
+            'uses-responsive-images': ['error', { maxLength: 4 }],
+          },
+        },
+      ],
     },
   },
 }
