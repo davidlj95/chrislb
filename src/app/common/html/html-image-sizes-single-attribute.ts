c2f9b626cbbb14fa69c26c148808378ce77f08fa
David LJ <mail@davidlj95.com>
2025-04-03 03:36:05 +0200

    refactor: remove lodash-es

--- src/app/common/html/html-image-sizes-single-attribute.ts
+++ src/app/common/html/html-image-sizes-single-attribute.ts
@@ -1,6 +1,5 @@
 import { CssMediaQuery } from '../css/css-media-query'
 import { CssUnit } from '../css/unit/css-unit'
-import { isUndefined } from 'lodash-es'
 
 export class HtmlImageSizesSingleAttribute {
   constructor(
@@ -9,7 +8,7 @@ export class HtmlImageSizesSingleAttribute {
   ) {}
 
   toString(): string {
-    if (isUndefined(this.mediaQuery)) {
+    if (this.mediaQuery === undefined) {
       return this.width.toString()
     }
     return [this.mediaQuery, this.width].join(' ')
--- src/app/common/routing/get-title.ts
+++ src/app/common/routing/get-title.ts
@@ -1,8 +1,7 @@
 import defaultMetadata from '@/data/cms/misc/metadata.json'
-import { isEmpty } from 'lodash-es'
 
 export function getTitle(pageTitle: string | undefined | null) {
-  if (isEmpty(pageTitle?.trim())) {
+  if (pageTitle?.trim()) {
     return defaultMetadata.siteName
   }
   return `${pageTitle} | ${defaultMetadata.siteName}`
