d172bc5c1d427f59f6ea8fed5a7f10db671341fd
David LJ <mail@davidlj95.com>
2025-04-04 00:24:33 +0200

    test: try enabling back most Lighthouse assertions

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -19,18 +19,7 @@ module.exports = {
           matchingUrlPattern: '.*',
           preset: 'lighthouse:no-pwa',
           assertions: {
-            //👇 Didn't appear til main bundle was >500kb. Then, Lighthouse wants source maps
-            // If we optimize, we can probably turn this back on. 500kb for main bundle is a bit too much
-            // ~Now we're below 500kb, enabling it again by leaving it commented in case it's bigger~
-            'valid-source-maps': 'off',
             'uses-responsive-images': ['error', { maxLength: 2 }],
-            // 👇 Probably Swiper.js 😭
-            // Just happens on project detail page tho, when many swipers there
-            // Maybe a grid would solve it
-            'non-composited-animations': 'warn',
-            'bf-cache': 'warn',
-            'csp-xss': 'warn',
-            'unused-javascript': 'warn',
           },
         },
         // Non-project detail
