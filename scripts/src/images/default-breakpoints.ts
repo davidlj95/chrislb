73392feeec62ac75820afbacc632a151b3d3cd4a
David LJ <mail@davidlj95.com>
2025-04-03 19:13:38 +0200

    chore: remove unneeded default breakpoints file [skip ci]

--- scripts/src/images/default-breakpoints.ts
+++
@@ -1,7 +0,0 @@
-// Based on `chiasma_101.jpg` and `responsivebreakpoints.com` results
-// Params: 200-1920. Step 20 KB. 20 Max images. No DPR.
-// Discarding resolutions > 1920 as high network bandwidth is assumed in there
-export const DEFAULT_BREAKPOINTS = [
-  200, 460, 719, 914, 1056, 1261, 1296, 1399, 1500, 1593, 1678, 1756, 1830,
-  1910, 1920,
-]
