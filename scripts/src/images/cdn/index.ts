afb329a36896351dd78935deda9162f2b7d7ea71
David LJ <mail@davidlj95.com>
2025-04-08 19:08:54 +0200

    refactor: rename CDN API files to be distinguished

--- scripts/src/images/cdn/index.ts
+++ scripts/src/images/cdn/index.ts
@@ -1,15 +1,15 @@
 // 👇 Needs to be here, first line.
 //    Because of image CDN config exporting Angular providers
 import '@angular/compiler'
-import { Cloudinary } from './apis/cloudinary'
-import { Imagekit } from './apis/imagekit'
+import { CloudinaryCdnApi } from './apis/cloudinary-cdn-api'
+import { ImagekitCdnApi } from './apis/imagekit-cdn-api'
 import { CDN_NAME, CdnNames } from '@/app/common/images/cdn'
 import { Log } from '../../utils/log'
 import type { ImageCdnApi } from './image-cdn-api'
 
 const CDN_APIS_BY_NAME: Record<CdnNames, () => Promise<ImageCdnApi>> = {
-  imagekit: async () => Imagekit.getInstance(),
-  cloudinary: Cloudinary.getInstance,
+  imagekit: async () => ImagekitCdnApi.getInstance(),
+  cloudinary: CloudinaryCdnApi.getInstance,
 }
 export const getImageCdnApi = (): Promise<ImageCdnApi> => {
   Log.info(`Using ${CDN_NAME} as image CDN`)
