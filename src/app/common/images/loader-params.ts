277f39304abdf103df73e005ae983d986917601a
David LJ <mail@davidlj95.com>
2025-04-13 22:37:20 +0200

    perf: remove return to save some bytes :P

--- src/app/common/images/loader-params.ts
+++ src/app/common/images/loader-params.ts
@@ -1,11 +1,9 @@
 import { Pipe, PipeTransform } from '@angular/core'
-import { areBreakpointsUnsigned, LoaderParams, ResponsiveImage } from './image'
+import { areBreakpointsUnsigned, ResponsiveImage } from './image'
 
 @Pipe({ name: 'toLoaderParams', standalone: true, pure: true })
 export class ToLoaderParams implements PipeTransform {
-  transform(image: ResponsiveImage): LoaderParams {
-    return toLoaderParams(image)
-  }
+  transform = toLoaderParams
 }
 
 export const toLoaderParams = ({ breakpoints }: ResponsiveImage) => ({
