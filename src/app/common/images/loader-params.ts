0edea38fe9d670a5c6ddcb983ae2b823913b2835
David LJ <mail@davidlj95.com>
2025-04-13 22:32:49 +0200

    refactor: implement own optimized image to avoid auto sizes

--- src/app/common/images/loader-params.ts
+++ src/app/common/images/loader-params.ts
@@ -4,11 +4,12 @@ import { areBreakpointsUnsigned, LoaderParams, ResponsiveImage } from './image'
 @Pipe({ name: 'toLoaderParams', standalone: true, pure: true })
 export class ToLoaderParams implements PipeTransform {
   transform(image: ResponsiveImage): LoaderParams {
-    const { breakpoints } = image
-    return {
-      signaturesByBreakpoint: areBreakpointsUnsigned(breakpoints)
-        ? undefined
-        : breakpoints,
-    }
+    return toLoaderParams(image)
   }
 }
+
+export const toLoaderParams = ({ breakpoints }: ResponsiveImage) => ({
+  signaturesByBreakpoint: areBreakpointsUnsigned(breakpoints)
+    ? undefined
+    : breakpoints,
+})
