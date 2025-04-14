11809f6ee9d4fc991f8c94b0aedf2decb9eebd0c
David LJ <mail@davidlj95.com>
2025-04-14 22:46:13 +0200

    feat: no canonical URL for 404 page

--- src/app/not-found-page/not-found-page.routes.ts
+++ src/app/not-found-page/not-found-page.routes.ts
@@ -2,11 +2,14 @@ import { Routes } from '@angular/router'
 import { NotFoundPageComponent } from './not-found-page.component'
 import notFoundPageMetadata from '@/data/cms/pages/404.json'
 import { NgxMetaRouteData } from '@davidlj95/ngx-meta/routing'
+import { GlobalMetadata } from '@davidlj95/ngx-meta/core'
 
 export const NOT_FOUND_PAGE_ROUTES: Routes = [
   {
     path: '',
     component: NotFoundPageComponent,
-    data: { meta: notFoundPageMetadata } satisfies NgxMetaRouteData,
+    data: {
+      meta: { ...notFoundPageMetadata, canonicalUrl: null },
+    } satisfies NgxMetaRouteData<GlobalMetadata>,
   },
 ]
