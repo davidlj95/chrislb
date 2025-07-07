2f1ec5716e2a5b5c57072a0fe6df8901d7c10d9c
David LJ <mail@davidlj95.com>
2025-07-08 01:47:19 +0200

    test: fix unit tests

--- src/test/testbed-setup.ts
+++ src/test/testbed-setup.ts
@@ -1,14 +1,11 @@
-import { provideExperimentalZonelessChangeDetection } from '@angular/core'
 import { TestBed } from '@angular/core/testing'
+import { provideZonelessChangeDetection } from '@angular/core'
 
 export const testbedSetup = (
   opts: Parameters<typeof TestBed.configureTestingModule>[0] = {},
 ) => {
   TestBed.configureTestingModule({
     ...opts,
-    providers: [
-      provideExperimentalZonelessChangeDetection(),
-      ...(opts.providers ?? []),
-    ],
+    providers: [provideZonelessChangeDetection(), ...(opts.providers ?? [])],
   })
 }
